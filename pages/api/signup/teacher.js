86acda0f38417b3527bed12202dd43dec7f85f09
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2022-06-30 18:42:23 +0600

    improved teacher insertion code

--- pages/api/signup/teacher.js
+++ pages/api/signup/teacher.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
 import { stat } from "fs";
+import { db } from "../../../config/db";
 
 export default async (req, res) => {
   try {
@@ -12,24 +13,32 @@ export default async (req, res) => {
     const uniqueId = uuidv4();
     console.log("req nom", req.body);
     const approval = "pending";
-    const result = await executeQuery({
-      query:
-        "INSERT INTO teachers VALUES('" +
-        uniqueId +
-        "','" +
-        req.body.name +
-        "',NULL,NULL,'" +
-        req.body.email +
-        "','" +
-        approval +
-        "',NULL,'" +
-        hashedPassword +
-        "','false')",
-    });
-    console.log("ttt", result);
-    res.send({
-      status: "successs",
-      message: "successfully registered teacher",
+    var sql =
+      "INSERT INTO teachers VALUES('" +
+      uniqueId +
+      "','" +
+      req.body.name +
+      "',NULL,NULL,'" +
+      req.body.email +
+      "','" +
+      approval +
+      "',NULL,'" +
+      hashedPassword +
+      "','false')";
+    db.query(sql, function (err, result) {
+      if (err) {
+        console.log(err);
+        return res.status(400).send({
+          status: "fail",
+          message: "try agaun",
+          errorMessage: err,
+        });
+      } else {
+        return res.send({
+          status: "successs",
+          message: "successfully registered teacher",
+        });
+      }
     });
   } catch (err) {
     console.log(err);
