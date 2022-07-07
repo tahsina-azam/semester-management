6289e38ce4274636ebd12d690177bea37515c597
nowshin alam owishi <owishibooo@pop-os.localdomain>
2022-07-07 22:55:46 +0600

    added teacher id

--- pages/teachers/index.tsx
+++ pages/teachers/index.tsx
@@ -1,12 +1,15 @@
+import { useEffect } from "react";
 import { useAuth } from "../../lib/client/context/auth";
 import AppShellWithRole from "../../src/components/common/Bars";
 
 export default function Student() {
   const { user } = useAuth();
-
+  useEffect(() => {
+    console.log(user.id)//id for teacher
+  }, []);
   return (
     <AppShellWithRole user={user}>
-      <div></div>
+      <div>{user.id}</div>
     </AppShellWithRole>
   );
 }
