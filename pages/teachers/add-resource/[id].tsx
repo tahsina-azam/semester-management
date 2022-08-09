be3f561ff538e8b825a7112e123ca054c2d0c132
nowshin alam owishi <owishibooo@pop-os.localdomain>
2022-08-09 22:14:50 +0600

    resource added

--- pages/teachers/add-resource/[id].tsx
+++ pages/teachers/add-resource/[id].tsx
@@ -3,71 +3,77 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import notify from "../../../src/components/common/Notifications";
 import { useRouter } from "next/router";
-import { Button, Card, Center, TextInput } from "@mantine/core";
-import { injectStyle } from "react-toastify/dist/inject-style";
-import { Input } from "semantic-ui-react";
+import { Button, Card, Center, LoadingOverlay, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { TypeButton } from "../../../src/components/common/Button";
+import ComposedButton, {
+  TypeButton,
+} from "../../../src/components/common/Button";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import storage from "../../../config/firebase";
+import { Input } from "semantic-ui-react";
 
-function uploadFile(file: File): Promise<any> {
-  return new Promise((resolve, reject) => {
-    if (!file) return;
-    const storageRef = ref(storage, `images/${file.name}`);
-    const uploadTask = uploadBytesResumable(storageRef, file);
-    uploadTask.on(
-      "state_changed",
-      (snapshot: any) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
-      },
-      (error: any) => {
-        reject(error);
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((url: any) => {
-          console.log(url);
-          resolve(url);
-        });
-      }
-    );
-  });
-}
+
 export default function () {
+  const [value, setValue] = useState(null);
+  const [valueText, setValueText] = useState("");
   const [visible, setVisible] = useState(false);
-  const router = useRouter();
-  const onSubmit = async (values) => {
-    const { description, file } = values;
-    const link = uploadFile(file);
-    console.log(link);
+  function uploadFile(file: File): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if (!file) return;
+      const storageRef = ref(storage, `files/${file.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot: any) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+        },
+        (error: any) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((url: any) => {
+            console.log(url);
+            setValue(url)
+            resolve(url);
+          });
+        }
+      );
+    });
+  }
+  const onChange = (e) => {
+      e.preventDefault()
+      setVisible(true);
+      console.log(e.target.files[0]);
+      const file = e.target.files[0];
+      uploadFile(file);
+      setVisible(false);
+  }
+  const onSubmit = async () => {
+    console.log({ value });
+    console.log({ valueText });
   };
-  const form = useForm({
-    initialValues: {
-      file: null,
-      description: "",
-    },
-  });
   return (
     <Center style={{ width: "100%", height: "100vh" }}>
       <Card withBorder style={{ width: "auto", height: "auto" }}>
         <Center>
-          <form onSubmit={form.onSubmit((values) => onSubmit(values))}>
-            <Center>
-              <Input type={"file"} required {...form.getInputProps("file")} />
-            </Center>
+          <LoadingOverlay visible={visible} />
+          <Input
+            type={"file"}
+            required
+            onChange={onChange}
+          />
+        </Center>
 
-            <TextInput
-              placeholder="Write about the file"
-              mt="xl"
-              style={{ width: "auto", height: "70%" }}
-              {...form.getInputProps("description")}
-            />
-            <Center mt={"xl"}>
-              <TypeButton />
-            </Center>
-          </form>
+        <TextInput
+          placeholder="Write about the file"
+          mt="xl"
+          style={{ width: "auto", height: "70%" }}
+          onChange={(e) => setValueText(e.target.value)}
+        />
+        <Center mt={"xl"}>
+          <ComposedButton text="submit" onClick={onSubmit} />
         </Center>
       </Card>
     </Center>
